e2587f5beb254384d0248810c11434796ae7d83c
Chloe Chan <74789888+chloec2@users.noreply.github.com>
2022-11-11 13:59:18 -0500

    added format checker into workflow (#2)
    * ran black to format files
    
    * added black formatter to yml
    
    * created new job instaed of putting it in same job

--- app/tests/test_fib.py
+++ app/tests/test_fib.py
@@ -1,5 +1,9 @@
 from app.fib import fibonacci
 
 # Test cases for the fibonacci function
-def test_first_fibonacci(): assert(fibonacci(1) == 1)
-def test_two_fibonacci(): assert(fibonacci(2) == 1)
+def test_first_fibonacci():
+    assert fibonacci(1) == 1
+
+
+def test_two_fibonacci():
+    assert fibonacci(2) == 1
